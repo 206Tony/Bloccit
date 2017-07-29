e241ec3f12cd315b73dec08f4c1def9a7069cad0
Anthony Zardis <anthonyzardis@gmail.com>
2017-07-28 17:21:08 -0700

    Assignment-27-Authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-# Examples:
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'random_data'
 include RandomData
 
@@ -76,6 +70,13 @@ member = User.create!(
 	name: "Kira",
 	email: "kira@bloccit.com",
 	password: 'password'
+)
+
+moderator = User.create!(
+	name: 'Moderator User',
+	email: "mod@example.com",
+	password: "helloworld",
+	#role: 'moderator'
 )
 
 
