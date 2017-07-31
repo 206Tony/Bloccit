13d8b772360838fdb61d762dac5a4f8d56a6af6e
Anthony Zardis <anthonyzardis@gmail.com>
2017-07-31 15:24:14 -0700

    Assignment-29-Voting

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -20,9 +20,13 @@ RSpec.describe Post, type: :model do
   it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
   describe "attributes" do
-  	it "has title and body attributes" do
-  		expect(post).to have_attributes(title: title, body: body)
+  	it "should respond to title" do
+  		expect(post).to respond_to(:title)
   	end
+
+    it "should respond to body" do
+      expect(post).to respond_to(:body)
+    end
   end
 
   describe "voting" do
@@ -44,6 +48,7 @@ RSpec.describe Post, type: :model do
         expect( post.down_votes ).to eq(@down_votes)
       end
     end
+    
     describe "#points" do
       it "returns the sum of all down and up votes" do
         expect( post.points ).to eq(@up_votes - @down_votes)
@@ -67,6 +72,22 @@ RSpec.describe Post, type: :model do
         post.votes.create!(value: -1, user: user)
         expect(post.rank).to eq(old_rank - 1)
       end
+    end
+  end
+
+  describe "#create_vote" do
+    it "sets the post up_votes to 1" do
+      expect(post.up_votes).to eq(1)
+    end
+
+    it "calls #create_vote when a post is created" do
+      post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+      expect(post).to receive(:create_vote)
+      post.save
+    end
+
+    it "associates the vote with the owner of the post" do
+      expect(post.votes.first.user).to eq(post.user)
     end
   end
 end
